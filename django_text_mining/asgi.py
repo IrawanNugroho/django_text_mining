66b4268b70f896fe759bb11a047875b757d1a1fd
Irawan Setyo Adhi Nugroho <irawansetyoadhi.nugroho3@gmail.com>
2020-11-13 07:13:27 +0700

    fresh install with main app
    Ternyata kalau mau akses langsung, nama project harus sama dengan nama directory. Nggak tau kalau beda, mungkin harus dideklarasikan dulu.

--- django_text_mining/asgi.py
+++ django_text_mining/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for text_mining project.
+ASGI config for django_text_mining project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'text_mining.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_text_mining.settings')
 
 application = get_asgi_application()
--- django_text_mining/wsgi.py
+++ django_text_mining/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for text_mining project.
+WSGI config for django_text_mining project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'text_mining.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_text_mining.settings')
 
 application = get_wsgi_application()
